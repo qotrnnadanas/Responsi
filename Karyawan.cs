9f993c3adc6a8f9a7317384e3caf248c87d97f71
qotrun <qotrunnafa@gmail.com>
2022-04-19 12:17:02 +0700

    res

---
+++ Karyawan.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace responsi
+{
+    class Karyawan
+    {
+        public Karyawan()
+        {
+
+        }
+        public Karyawan(string nama, string nik, int gajibulanan)
+        {
+            NAMA = nama;
+            NIK = nik;
+            GajiBulanan = gajibulanan;
+        }
+        public string NAMA { get; set; }
+        public string NIK { get; set; }
+        public int GajiBulanan { get; set; }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace responsi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Karyawan karyawan1 = new Karyawan();
+
+            karyawan1.NIK = "21114354";
+            karyawan1.NAMA = "Qotrun";
+            karyawan1.GajiBulanan = 3000000;
+
+            Karyawan karyawan2 = new Karyawan();
+
+            karyawan2.NIK = "12332112";
+            karyawan2.NAMA = "Nasrulloh";
+            karyawan2.GajiBulanan = 2000000;
+
+            Console.WriteLine("No  Nik\t\tNama\tGaji Bulanan");
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine("1. {0} {1}\t{2}", karyawan1.NIK, karyawan1.NAMA, karyawan1.GajiBulanan);
+            Console.WriteLine("2. {0} {1}\t{2}", karyawan2.NIK, karyawan2.NAMA, karyawan2.GajiBulanan);
+            Console.WriteLine("\n");
+            Console.WriteLine("asyiiiikkkkk kenaikan gaji 10%");
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine("1. {0} {1}\t{2}", karyawan1.NIK, karyawan1.NAMA, karyawan1.GajiBulanan + (karyawan1.GajiBulanan * 0.1));
+            Console.WriteLine("2. {0} {1}\t{2}", karyawan2.NIK, karyawan2.NAMA, karyawan2.GajiBulanan + (karyawan2.GajiBulanan * 0.1));
+            Console.ReadKey();
+        }
+    }
+}
